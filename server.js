94185d1c17082cb47f5fe093d1c8b2aa0f31dc0e
Pawel Godula <bolomaster7@gmail.com>
2023-03-06 18:36:29 +0100

    before replit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const uuid = require('uuid').v4;
 const router = express.Router();
-;
+const path = require('path');
 
 const app = express();
 
@@ -15,14 +15,19 @@ const seatsRoutes = require('./routes/seats.routes.js');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api/', testimonialRoutes);
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
-app.listen(8000, () => {
+app.listen(process.env.PORT  || 8000, () => {
     console.log('Server is running on port: 8000');
 });
